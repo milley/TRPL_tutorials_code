5be8ac1cc779f3498e5b1b1b3ede1c074c903a11
milley <milley.gm@gmail.com>
2021-12-25 22:36:30 +0800

    chapter13.1 closure basic

---
+++ 13_functional_features/13_closure_demo/src/main.rs
@@ -0,0 +1,94 @@
+#![allow(dead_code)]
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+// fn generate_workout(intensity: u32, random_number: u32) {
+//     if intensity < 25 {
+//         println!(
+//             "Today, do {} pushups!",
+//             simulated_expensive_calculation(intensity)
+//         );
+//         println!(
+//             "Next, do {} situps!",
+//             simulated_expensive_calculation(intensity)
+//         );
+//     } else {
+//         if random_number == 3 {
+//             println!("Take a break today! Remeber to say hydrated!");
+//         } else {
+//             println!(
+//                 "Today, run for {} minutes!",
+//                 simulated_expensive_calculation(intensity)
+//             );
+//         }
+//     }
+// }
+
+
+// fn generate_workout(intensity: u32, random_number: u32) {
+//     let expensive_result = simulated_expensive_calculation(intensity);
+
+//     if intensity < 25 {
+//         println!(
+//             "Today, do {} pushups!",
+//             expensive_result
+//         );
+//         println!(
+//             "Next, do {} situps!",
+//             expensive_result
+//         );
+//     } else {
+//         if random_number == 3 {
+//             println!("Take a break today! Remeber to say hydrated!");
+//         } else {
+//             println!(
+//                 "Today, run for {} minutes!",
+//                 expensive_result
+//             );
+//         }
+//     }
+// }
+
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+    let expensive_result = expensive_closure(intensity);
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remeber to say hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result
+            );
+        }
+    }
+}
