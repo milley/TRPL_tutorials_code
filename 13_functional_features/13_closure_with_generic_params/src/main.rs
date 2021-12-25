231a233a6676cbafa1c6f941f69888f629a25580
milley <milley.gm@gmail.com>
2021-12-25 23:10:22 +0800

    chapter13.4 closure capture

--- 13_functional_features/13_closure_with_generic_params/src/main.rs
+++ 13_functional_features/13_closure_with_generic_params/src/main.rs
@@ -35,6 +35,21 @@ fn main() {
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
 
+
+    let x = 4;
+    let equal_to_x = |z| z == x;
+    // fn equal_to_x(z: i32) -> bool {
+    //     z == x
+    // }
+    let y = 4;
+    assert!(equal_to_x(y));
+
+
+    let x = vec![1, 2, 3];
+    let equal_to_x = move |z| z == x;
+    // println!("Can't use x here: {:?}", x);
+    let y = vec![1, 2, 3];
+    assert!(equal_to_x(y));
 }
 
 
