ac8e176157f7fa133da825675587f2998f13ebab
milley <milley.gm@gmail.com>
2021-12-25 21:26:23 +0800

    chapter12.3 refactor 1st

--- 12_command_program/12_mini_grep/src/main.rs
+++ 12_command_program/12_mini_grep/src/main.rs
@@ -1,16 +1,27 @@
 use std::{env, fs};
 
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Search for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
     
     println!("With text:\n{}", contents);
 }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
+}
+
