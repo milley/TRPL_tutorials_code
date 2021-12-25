5d81f09d70c41b50760c976918aca9c3ffd9bdbf
milley <milley.gm@gmail.com>
2021-12-25 23:17:35 +0800

    chapter13.5 closure iter

---
+++ 13_functional_features/13_closure_iter/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+    for val in v1_iter {
+        println!("got: {}", val);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+    }
+}
--- 13_functional_features/13_closure_with_generic_params/src/main.rs
+++ 13_functional_features/13_closure_with_generic_params/src/main.rs
@@ -88,7 +88,7 @@ mod tests {
     #[test]
     fn call_with_different_values() {
         let mut c = super::Cacher::new(|a| a);
-        let v1 = c.value(1);
+        let _v1 = c.value(1);
         let v2 = c.value(2);
 
         assert_eq!(v2, 2);
