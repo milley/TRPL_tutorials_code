2ffb470cdba73e5e312e1343ab01888427863000
milley <milley.gm@gmail.com>
2021-12-26 13:38:56 +0800

    chapter15.5 refcell trait

--- 15_smart_point/15_rc_trait_demo/src/main.rs
+++ 15_smart_point/15_rc_trait_demo/src/main.rs
@@ -13,11 +13,11 @@ fn main() {
     println!("count after creating a = {}", Rc::strong_count(&a));
 
     
-    let b = Cons(3, Rc::clone(&a));
+    let _b = Cons(3, Rc::clone(&a));
     println!("count after creating b = {}", Rc::strong_count(&a));
 
     {
-        let c = Cons(4, Rc::clone(&a));
+        let _c = Cons(4, Rc::clone(&a));
         println!("count after creating c = {}", Rc::strong_count(&a));
     }
     println!("count after c goes out of scope = {}", Rc::strong_count(&a));
