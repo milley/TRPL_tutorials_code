aedc76f5aeef479e50f742ac4325c9c235e64162
milley <milley.gm@gmail.com>
2021-12-27 13:14:15 +0800

    chapter20 web server

--- 20_multi_thread_web_server/hello/src/main.rs
+++ 20_multi_thread_web_server/hello/src/main.rs
@@ -3,15 +3,25 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 
 use std::fs;
+use std::time::Duration;
+use std::thread;
+use hello::ThreadPool;
 
 fn main() {
     let listen = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    for stream in listen.incoming() {
+    let pool = ThreadPool::new(4);
+
+    for stream in listen.incoming().take(2) {
+
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -20,8 +30,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
@@ -37,30 +51,4 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
-
-    // if buffer.starts_with(get) {
-    //     let contents = fs::read_to_string("hello.html").unwrap();
-    //     let response = format!(
-    //         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-    //         contents.len(),
-    //         contents
-    //     );
-
-    //     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    //     stream.write(response.as_bytes()).unwrap();
-    //     stream.flush().unwrap();
-    // } else {
-    //     let status_line = "HTTP/1.1 404 NOT FOUND";
-    //     let contents = fs::read_to_string("404.html").unwrap();
-
-    //     let response = format!(
-    //         "{}\r\nContent-Length: {}\r\n\r\n{}",
-    //         status_line,
-    //         contents.len(),
-    //         contents
-    //     );
-
-    //     stream.write(response.as_bytes()).unwrap();
-    //     stream.flush().unwrap();
-    // }
 }
