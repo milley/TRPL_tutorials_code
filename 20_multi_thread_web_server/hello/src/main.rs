cde3cf46f37e31bf70ab1294c09a4a4e5c63d8fd
milley <milley.gm@gmail.com>
2021-12-27 12:16:48 +0800

    chapter20.1 single thread server

---
+++ 20_multi_thread_web_server/hello/src/main.rs
@@ -0,0 +1,66 @@
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+use std::fs;
+
+fn main() {
+    let listen = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listen.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+    // if buffer.starts_with(get) {
+    //     let contents = fs::read_to_string("hello.html").unwrap();
+    //     let response = format!(
+    //         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+    //         contents.len(),
+    //         contents
+    //     );
+
+    //     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    //     stream.write(response.as_bytes()).unwrap();
+    //     stream.flush().unwrap();
+    // } else {
+    //     let status_line = "HTTP/1.1 404 NOT FOUND";
+    //     let contents = fs::read_to_string("404.html").unwrap();
+
+    //     let response = format!(
+    //         "{}\r\nContent-Length: {}\r\n\r\n{}",
+    //         status_line,
+    //         contents.len(),
+    //         contents
+    //     );
+
+    //     stream.write(response.as_bytes()).unwrap();
+    //     stream.flush().unwrap();
+    // }
+}
